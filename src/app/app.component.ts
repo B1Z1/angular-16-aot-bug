e90c71130c6a0cebcee5721317e9438753723d19
Illia Mishkin <illia.mishkin@softwareplant.com>
2023-06-13 11:35:07 +0200

    Add bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private static readonly OPTION = 'None';
+  private static readonly OPTION_TEXT = Symbol(AppComponent.OPTION);
+
   title = 'aot-bug';
+
+  constructor() {
+    console.log(AppComponent.OPTION_TEXT);
+  }
 }
